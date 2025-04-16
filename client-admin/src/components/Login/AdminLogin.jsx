6d1b8d89a566671873e97e427936b42b3b2ab6af
alexanderjuxoncobb <alexanderjuxoncobb@gmail.com>
2025-04-16 18:19:14 +0100

    Change link to a button and rel path

--- client-admin/src/components/Login/AdminLogin.jsx
+++ client-admin/src/components/Login/AdminLogin.jsx
@@ -142,10 +142,7 @@ function AdminLogin({ redirectPath = "/" }) {
             Admin access only. Regular users should use the main site login.
           </p>
           <button
-            onClick={() =>
-              (window.location.href =
-                "https://blog-api-top-production.up.railway.app/")
-            }
+            onClick={() => (window.location.href = "/")}
             className="text-sky-600 hover:text-sky-800 mt-2 inline-block transition-colors"
           >
             Return to main site
